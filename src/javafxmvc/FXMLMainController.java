9efb65359444058494ec5f371d73e264dc710e57
IPTV PlayOn <jonatanbandeira21@gmail.com>
2021-06-25 17:45:57 -0300

    Menu Navebar

--- src/javafxmvc/FXMLMainController.java
+++ src/javafxmvc/FXMLMainController.java
@@ -3,16 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxmvc.controller;
+package javafxmvc;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -45,47 +48,51 @@ public class FXMLMainController implements Initializable {
 
     @FXML
     private void page1(MouseEvent event) {
-        loadPage("page1");
+        loadPage("page1.fxml");
     }
 
     @FXML
     private void page2(MouseEvent event) {
-        loadPage("page2");
+        loadPage("page2.fxml");
     }
 
     @FXML
     private void page3(MouseEvent event) {
-        loadPage("page3");
+        loadPage("page3.fxml");
     }
 
     @FXML
     private void page4(MouseEvent event) {
-        loadPage("page4");
+        loadPage("page4.fxml");
     }
 
     @FXML
     private void page5(MouseEvent event) {
-        loadPage("page5");
+        loadPage("page5.fxml");
     }
 
     @FXML
     private void page6(MouseEvent event) {
-        loadPage("page6");
+        loadPage("page6.fxml");
     }
 
     @FXML
     private void page7(MouseEvent event) {
-        loadPage("page7");
+        loadPage("page7.fxml");
     }
     
     private void loadPage(String page){
         Parent root = null;
         
-         try {
-            root =javafx.fxml.FXMLLoader.load(getClass().getResource(page+".fxml"));
+         
+        try {
+            root =javafx.fxml.FXMLLoader.load(getClass().getResource(page));
         } catch (IOException ex) {
             Logger.getLogger(FXMLMainController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        
+        
         
         bp.setCenter(root);
     }
